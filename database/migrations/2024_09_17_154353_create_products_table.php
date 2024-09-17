00af3a1f2100cd624a6ff9049e5b093f38fc2427
llite22 <llite2@yandex.ru>
2024-09-17 17:35:40 +0300

    fix category_id

--- database/migrations/2024_09_17_154353_create_products_table.php
+++ database/migrations/2024_09_17_154353_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('category_id')->nullable();
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
